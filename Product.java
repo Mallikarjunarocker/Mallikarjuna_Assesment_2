ee140c61076d84eb510163daf65298ca6879271c
Mallikarjunarocker <84621302+Mallikarjunarocker@users.noreply.github.com>
2022-09-02 00:20:09 +0530

    Add files via upload

---
+++ Product.java
@@ -0,0 +1,56 @@
+package com.itc.main.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int pid;
+	private String pname;
+	private double price;
+	 
+
+	public Product() {
+     super();
+	}
+	
+
+	public Product(int pid, String pname, double price) {
+		super();
+		this.pid = pid;
+		this.pname = pname;
+		this.price = price;
+	}
+
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+}
